05adc208ed5584b8ca24b0868dfd473e3235815d
Letterus <letterus@codingpastor.de>
2023-01-16 12:28:24 +0100

    Add manual fix for OGPango

--- OGPango/src/OGPangoCoverage.h
+++ OGPango/src/OGPangoCoverage.h
@@ -6,6 +6,13 @@
 
 #include <pango/pango.h>
 
+#define PANGO_TYPE_COVERAGE              (pango_coverage_get_type ())
+#define PANGO_COVERAGE(object)           (G_TYPE_CHECK_INSTANCE_CAST ((object), PANGO_TYPE_COVERAGE, PangoCoverage))
+#define PANGO_IS_COVERAGE(object)        (G_TYPE_CHECK_INSTANCE_TYPE ((object), PANGO_TYPE_COVERAGE))
+#define PANGO_COVERAGE_CLASS(klass)      (G_TYPE_CHECK_CLASS_CAST ((klass), PANGO_TYPE_COVERAGE, PangoCoverageClass))
+#define PANGO_IS_COVERAGE_CLASS(klass)   (G_TYPE_CHECK_CLASS_TYPE ((klass), PANGO_TYPE_COVERAGE))
+#define PANGO_COVERAGE_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS ((obj), PANGO_TYPE_COVERAGE, PangoCoverageClass))
+
 #import <OGObject/OGObject.h>
 
 /**
